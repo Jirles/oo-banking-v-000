cf7a8ac40ed40ee74e269d0b3458c492f3657935
Emily Jirles <emily.jirles@gmail.com>
2018-05-02 00:27:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,11 @@
 class Transfer
   
+  attr_accessor :sender, :recipient, :amt
   
+  def initialize(sender, recipient, amt)
+    @sender = sender 
+    @recipient = recipient
+    @amt = amt
+  end
   
 end
