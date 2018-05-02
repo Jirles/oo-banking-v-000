30b059b35dbb41a6359eb457e0244d01a73b64fb
Emily Jirles <emily.jirles@gmail.com>
2018-05-02 00:55:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.status == ('pending'|'reversed') && self.valid?
+    if self.status == ('pending'||'reversed') && self.valid?
       self.sender.balance = self.sender.balance - self.amount
       self.receiver.balance = self.receiver.balance + self.amount
       self.status = 'complete'
